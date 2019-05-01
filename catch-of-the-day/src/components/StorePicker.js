7a8ff79f2b254c70b2cf7efa5991588157d719d3
Hunter Callaway <HunterCallaway@hotmail.com>
2019-05-01 15:52:42 -0600

    Added JSX to the return.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class StorePicker extends React.Component {
 	render() {
-		return <p>This is the Store Picker.</p>
+		return (
+			<Fragment>
+				<p>Soy Fish!</p>
+				{ /* Sample JSX comment -- it must go inside of the returned element */ }
+				<form className="store-selector">
+					<h2>Please select a store.</h2>
+					<input type="text" required placeholder="Store Name" />
+					<button type="Submit">Visit the store.</button>
+				</form>
+			</Fragment>
+			)
 	}
 }
 
