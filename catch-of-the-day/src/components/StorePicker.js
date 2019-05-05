ac5de547bb77f4751f21338bd0bf2b20e54694e2
Hunter Callaway <HunterCallaway@hotmail.com>
2019-05-04 22:28:54 -0600

    Imported the getFunName function from helpers.js file into the StorePicker component.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,35 @@ import React, { Fragment } from "react";
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+	//Constructor example
+/*	constructor() {
+		super();
+		this.goToStore = this.goToStore.bind(this);
+}*/
+
+	myInput = React.createRef();
+
+	goToStore = event => {
+		//1. Prevent the form from being submitted.
+		event.preventDefault();
+		//2. Get the text from the input.
+		console.log(this);
+		//3. Change the page to /store/whatever-the-user-entered.
+	};
+
 	render() {
 		return (
 			<Fragment>
 				{ /* Sample JSX comment -- it must go inside of the returned element */ }
-				<form className="store-selector">
+				<form className="store-selector" onSubmit={this.goToStore}>
 					<h2>Please select a store.</h2>
-					<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+					<input
+						type="text"
+						ref={this.myInput}
+						required
+						placeholder="Store Name"
+						defaultValue={getFunName()} />
 					<button type="Submit">Visit the store.</button>
 				</form>
 			</Fragment>
