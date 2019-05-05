41e6f9be4af66e402d363a832ad330f1bd385f9d
Hunter Callaway <HunterCallaway@hotmail.com>
2019-05-05 17:01:36 -0600

    Added double quotes to the getFunName import statement. Deleted the commented-out constructor. Used the new syntax with the ref. Used a props.history.push() method to update the routing. Reformatted the return statement in the render() function.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,22 +1,20 @@
 import React, { Fragment } from "react";
-import { getFunName } from '../helpers';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-
-	//Constructor example
-/*	constructor() {
-		super();
-		this.goToStore = this.goToStore.bind(this);
-}*/
-
 	myInput = React.createRef();
 
 	goToStore = event => {
 		//1. Prevent the form from being submitted.
 		event.preventDefault();
 		//2. Get the text from the input.
-		console.log(this);
+		/*
+		The following note is taken from the React for Beginners Slack channel:
+		If you are having issues with `this.nameRef.value.value` (the old API) being undefined then please try using `this.nameRef.current.value` (the new API). The starter files for the course have been updated to use `React 16.6.3` which uses the new API.
+		*/
+		const storeName = this.myInput.current.value;
 		//3. Change the page to /store/whatever-the-user-entered.
+		this.props.history.push(`/store/${storeName}`);
 	};
 
 	render() {
@@ -30,11 +28,12 @@ class StorePicker extends React.Component {
 						ref={this.myInput}
 						required
 						placeholder="Store Name"
-						defaultValue={getFunName()} />
-					<button type="Submit">Visit the store.</button>
+						defaultValue={getFunName()}
+					/>
+					<button type="submit">Visit the store.</button>
 				</form>
 			</Fragment>
-			)
+			);
 	}
 }
 
