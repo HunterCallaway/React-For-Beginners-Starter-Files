e2c9b8858fa5f837006bb76975f86b7dbb65c64a
Hunter Callaway <HunterCallaway@hotmail.com>
2019-05-06 14:51:42 -0600

    Created the Fish component. Imported the Fish component into the App component and added a ul tag with an Object.keys method.

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Fish extends React.Component {
+	render() {
+		return (
+			<li className="menu-fish">
+				<p>I'm a fish!</p>
+			</li>
+		);
+	}
+}
+
+export default Fish;
